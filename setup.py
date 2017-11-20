570e7978ba01c93118776899f7926ecb8147753b
Todd <myoung44@vols.utk.edu>
2017-11-20 10:14:08 -0500

    Wrapping :gift:

---
+++ setup.py
@@ -0,0 +1,16 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name = 'convae_pyro',
+    packages = ['convae_pyro'],
+    install_requires=['numpy', 'pytorch', 'pyro', 'matplotlib', 'seaborn'],
+    version = '0.0.1',
+    description = 'A convolutional VAE learned through variational inference',
+    author = 'Todd Young',
+    author_email = 'young.todd.mk@gmail.com',
+    url = 'https://github.com/yngtodd/convae_pyro',
+    keywords = ['variational inference', 'convolution', 'variational autoencoder'],
+)
