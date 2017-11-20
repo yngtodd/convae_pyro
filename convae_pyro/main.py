3dc61b6a09dc449cbd7ffe736b8d898d66427e37
Todd <myoung44@vols.utk.edu>
2017-11-20 10:49:51 -0500

    Main squeeze.

---
+++ convae_pyro/main.py
@@ -0,0 +1,113 @@
+import argparse
+import numpy as np
+import torch
+import visdom
+from torch.autograd import Variable
+
+import pyro
+import pyro.distributions as dist
+from pyro.infer import SVI
+from pyro.optim import Adam
+from pyro.util import ng_zeros, ng_ones
+
+from vae_plots import plot_llk, mnist_test_tsne, plot_vae_samples
+from mnist_cached import MNISTCached as MNIST
+from mnist_cached import setup_data_loaders
+
+
+
+def main():
+    # parse command line arguments
+    parser = argparse.ArgumentParser(description="parse args")
+    parser.add_argument('-n', '--num-epochs', default=101, type=int, help='number of training epochs')
+    parser.add_argument('-tf', '--test-frequency', default=5, type=int, help='how often we evaluate the test set')
+    parser.add_argument('-lr', '--learning-rate', default=1.0e-3, type=float, help='learning rate')
+    parser.add_argument('-b1', '--beta1', default=0.95, type=float, help='beta1 adam hyperparameter')
+    parser.add_argument('--cuda', action='store_true', default=False, help='whether to use cuda')
+    parser.add_argument('-visdom', '--visdom_flag', default=False, help='Whether plotting in visdom is desired')
+    parser.add_argument('-i-tsne', '--tsne_iter', default=100, type=int, help='epoch when tsne visualization runs')
+    args = parser.parse_args()
+
+    # setup MNIST data loaders
+    # train_loader, test_loader
+    train_loader, test_loader = setup_data_loaders(MNIST, use_cuda=args.cuda, batch_size=256)
+
+    # setup the VAE
+    vae = VAE(use_cuda=args.cuda)
+
+    # setup the optimizer
+    adam_args = {"lr": args.learning_rate}
+    optimizer = Adam(adam_args)
+
+    # setup the inference algorithm
+    svi = SVI(vae.model, vae.guide, optimizer, loss="ELBO")
+
+    # setup visdom for visualization
+    if args.visdom_flag:
+        vis = visdom.Visdom()
+
+    train_elbo = []
+    test_elbo = []
+    # training loop
+    for epoch in range(args.num_epochs):
+        # initialize loss accumulator
+        epoch_loss = 0.
+        # do a training epoch over each mini-batch x returned
+        # by the data loader
+        for _, (x, _) in enumerate(train_loader):
+            # if on GPU put mini-batch into CUDA memory
+            if args.cuda:
+                x = x.cuda()
+            # wrap the mini-batch in a PyTorch Variable
+            x = Variable(x)
+            # do ELBO gradient and accumulate loss
+            epoch_loss += svi.step(x)
+
+        # report training diagnostics
+        normalizer_train = len(train_loader.dataset)
+        total_epoch_loss_train = epoch_loss / normalizer_train
+        train_elbo.append(total_epoch_loss_train)
+        print("[epoch %03d]  average training loss: %.4f" % (epoch, total_epoch_loss_train))
+
+        if epoch % args.test_frequency == 0:
+            # initialize loss accumulator
+            test_loss = 0.
+            # compute the loss over the entire test set
+            for i, (x, _) in enumerate(test_loader):
+                # if on GPU put mini-batch into CUDA memory
+                if args.cuda:
+                    x = x.cuda()
+                # wrap the mini-batch in a PyTorch Variable
+                x = Variable(x)
+                # compute ELBO estimate and accumulate loss
+                test_loss += svi.evaluate_loss(x)
+
+                # pick three random test images from the first mini-batch and
+                # visualize how well we're reconstructing them
+                if i == 0:
+                    if args.visdom_flag:
+                        plot_vae_samples(vae, vis)
+                        reco_indices = np.random.randint(0, x.size(0), 3)
+                        for index in reco_indices:
+                            test_img = x[index, :]
+                            reco_img = vae.reconstruct_img(test_img)
+                            vis.image(test_img.contiguous().view(28, 28).data.cpu().numpy(),
+                                      opts={'caption': 'test image'})
+                            vis.image(reco_img.contiguous().view(28, 28).data.cpu().numpy(),
+                                      opts={'caption': 'reconstructed image'})
+
+            # report test diagnostics
+            normalizer_test = len(test_loader.dataset)
+            total_epoch_loss_test = test_loss / normalizer_test
+            test_elbo.append(total_epoch_loss_test)
+            print("[epoch %03d]  average test loss: %.4f" % (epoch, total_epoch_loss_test))
+
+        if epoch == args.tsne_iter:
+            mnist_test_tsne(vae=vae, test_loader=test_loader)
+            plot_llk(np.array(train_elbo), np.array(test_elbo))
+
+    return vae
+
+
+if __name__ == '__main__':
+    model = main()
